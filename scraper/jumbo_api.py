cb3b70bcf7a3650873a58b2817eb915b74581876
Lena <78238929+lenzdz@users.noreply.github.com>
2026-09-24 12:59:23 -0500

    Updated products

--- scraper/jumbo_api.py
+++ scraper/jumbo_api.py
@@ -4,87 +4,22 @@ import requests
 
 from notifiers.discord import enviar_mensaje_canal_errores
 
-# BASE_URL = "https://www.jumbocolombia.com/_v/segment/graphql/v1"
-
-
-# def obtener_flags_producto(product_id):
-#     # 1. Crear {"id":98321}
-#     variables = json.dumps(
-#         {"id": product_id},
-#         separators=(",", ":")
-#     )
-
-#     # 2. Convertir a Base64
-#     variables_b64 = base64.b64encode(
-#         variables.encode("utf-8")
-#     ).decode("utf-8")
-
-#     # 3. Crear el objeto extensions
-#     extensions = {
-#         "persistedQuery": {
-#             "version": 1,
-#             "sha256Hash": "7c16412d187093332665a3e33f79165687dbedbe291daa6842655678493fd1fd",
-#             "sender": "tiendasjumboqaio.jumbo-minicart@2.x",
-#             "provider": "vtex.search-graphql@0.x"
-#         },
-#         "variables": variables_b64
-#     }
-
-#     params = {
-#         "workspace": "master",
-#         "maxAge": "short",
-#         "appsEtag": "remove",
-#         "domain": "store",
-#         "locale": "es-CO",
-#         "operationName": "getProductInfo",
-#         "variables": "{}",
-#         "extensions": json.dumps(
-#             extensions,
-#             separators=(",", ":")
-#         )
-#     }
-
-#     headers = {
-#         "User-Agent": "Mozilla/5.0",
-#         "Accept": "application/json"
-#     }
-
-#     respuesta = requests.get(
-#         BASE_URL,
-#         params=params,
-#         headers=headers
-#     )
-
-#     # print("URL generada:")
-#     # print(respuesta.url)
-#     # print()
-
-#     # print("Status:", respuesta.status_code)
-
-#     #print(respuesta.text)
-#     return respuesta.json()
-
-BASE_URL = "https://www.jumbocolombia.com/_v/public/graphql/v1"
-
-
-import base64
-import json
-import requests
-
 BASE_URL = "https://www.jumbocolombia.com/_v/segment/graphql/v1"
 
 
 def obtener_producto(product_id):
-
+    # 1. Crear {"id":98321}
     variables = json.dumps(
-        {"id": int(product_id)},
+        {"id": product_id},
         separators=(",", ":")
     )
 
+    # 2. Convertir a Base64
     variables_b64 = base64.b64encode(
         variables.encode("utf-8")
     ).decode("utf-8")
 
+    # 3. Crear el objeto extensions
     extensions = {
         "persistedQuery": {
             "version": 1,
@@ -101,7 +36,6 @@ def obtener_producto(product_id):
         "appsEtag": "remove",
         "domain": "store",
         "locale": "es-CO",
-        "__bindingId": "2aad81c0-c729-41f4-a13b-002deae8039a",
         "operationName": "getProductInfo",
         "variables": "{}",
         "extensions": json.dumps(
@@ -118,17 +52,80 @@ def obtener_producto(product_id):
     respuesta = requests.get(
         BASE_URL,
         params=params,
-        headers=headers,
-        timeout=30
+        headers=headers
     )
 
-    # print("URL generada:")
-    # print(respuesta.url)
-    # print()
+    print("URL generada:")
+    print(respuesta.url)
+    print()
 
-    # print("Status:", respuesta.status_code)
+    print("Status:", respuesta.status_code)
 
+    print(respuesta.text)
     return respuesta.json()
+
+# import base64
+# import json
+# import requests
+
+# BASE_URL = "https://www.jumbocolombia.com/_v/segment/graphql/v1"
+
+
+# def obtener_producto(product_id):
+
+#     variables = json.dumps(
+#         {"id": int(product_id)},
+#         separators=(",", ":")
+#     )
+
+#     variables_b64 = base64.b64encode(
+#         variables.encode("utf-8")
+#     ).decode("utf-8")
+
+#     extensions = {
+#         "persistedQuery": {
+#             "version": 1,
+#             "sha256Hash": "7c16412d187093332665a3e33f79165687dbedbe291daa6842655678493fd1fd",
+#             "sender": "tiendasjumboqaio.jumbo-minicart@2.x",
+#             "provider": "vtex.search-graphql@0.x"
+#         },
+#         "variables": variables_b64
+#     }
+
+#     params = {
+#         "workspace": "master",
+#         "maxAge": "short",
+#         "appsEtag": "remove",
+#         "domain": "store",
+#         "locale": "es-CO",
+#         "__bindingId": "2aad81c0-c729-41f4-a13b-002deae8039a",
+#         "operationName": "getProductInfo",
+#         "variables": "{}",
+#         "extensions": json.dumps(
+#             extensions,
+#             separators=(",", ":")
+#         )
+#     }
+
+#     headers = {
+#         "User-Agent": "Mozilla/5.0",
+#         "Accept": "application/json"
+#     }
+
+#     respuesta = requests.get(
+#         BASE_URL,
+#         params=params,
+#         headers=headers,
+#         timeout=30
+#     )
+
+#     print("URL generada:")
+#     print(respuesta.url)
+#     print()
+
+#     print("Status:", respuesta.status_code)
+
+#     return respuesta.json()
 
 def obtener_descuento(id_producto, info_producto):
     try:
